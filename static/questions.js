b76e9bf7bcaf309dd39a3373c2f6ff7200915ce2
Andy Chen <andy.chen121@imperial.ac.uk>
2024-11-18 12:12:57 +0000

    question sheet creation and answer display

---
+++ static/questions.js
@@ -0,0 +1,30 @@
+const exercise = 
+{
+    "topic": "Data structure",
+    "mcq": [
+        {
+            "q": "What is the radius of the earth in kilometers?",
+            "multi": false,
+            "choices": ["12742", "6371", "384400", "3210"],
+            "ans": "B"
+        },
+        {
+            "q": "What of the following variable names are correct?",
+            "multi": true,
+            "choices": ["radius_2", "4_seasons", "R", "_qnum", "z-index"],
+            "ans": "ACD"
+        }
+    ],
+    "written": [
+        {   
+            "intro": "Complete the Python code to add numbers 0 to 9 to an array",
+            "questions": [
+                {"q": "arr = []<br>for i ", "a": [1, 10]},
+                {"q": " range(10): <br>&nbsp;&nbsp;&nbsp;arr. ", "a": [1, 10]},
+            ],
+            "rem": " (i)<br>return arr",
+            "ans": ["range", "append"]
+        }
+    ]
+
+}
